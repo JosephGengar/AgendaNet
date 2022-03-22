bf411c16023e61b26d48219583cede07e9d8c261
Joseph <josewsq101@gmail.com>
2022-03-22 19:59:43 -0300

    cambio de datatable a object

--- NetAgenda/2-Logica/Contacto.cs
+++ NetAgenda/2-Logica/Contacto.cs
@@ -9,6 +9,14 @@ namespace _2_Logica
 {
     public class Contacto : Persona
     {
+        private int _codigo;
+
+        public int codigo
+        {
+            get { return _codigo; }
+            set { _codigo = value; }
+        }
+
         private List<string> _numerosDeContacto;
 
         public List<string> numerosDeContacto
@@ -24,8 +32,33 @@ namespace _2_Logica
 
         public DataTable ListarTabla()
         {
+            //lo utiliz0 como muestra en el datagridview
             ContactoBD oContacto = new ContactoBD();
             return oContacto.listar();
+        }
+        public List<Contacto> Listar()
+        {
+            //como voy a tener coleccion d contactos lo utilizo en el combobox para tener mas plasticidad
+            List<Contacto> ListaRetorno = new List<Contacto>();
+            Contacto oContacto;
+            foreach (DataRow item in ListarTabla().Rows)
+            {
+                oContacto = new Contacto();
+                oContacto.nombre = item[1].ToString();
+                oContacto.apellido = item["apellido"].ToString();
+                oContacto.codigo = int.Parse(item[0].ToString());
+
+                ListaRetorno.Add(oContacto);
+            }
+
+            return ListaRetorno;
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            //en lugar de informar al display member, como quiero mas datos sobrescribo el tostring()
+            return string.Format("{0} - {1}, {2}", this.codigo, this.apellido, this.nombre);
         }
     }
 }
--- NetAgenda/NetAgenda/Form1.cs
+++ NetAgenda/NetAgenda/Form1.cs
@@ -20,25 +20,31 @@ namespace NetAgenda
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-
-            try
+            if (txtNombre.Text != "" | txtApellido.Text != "")
             {
-                Contacto oContacto = new Contacto();
-                oContacto.nombre = txtNombre.Text;
-                oContacto.apellido = txtApellido.Text;
-                if (oContacto.Guardar())
+                try
                 {
-                    MessageBox.Show("El Contacto se agrego con exito!!");
+                    Contacto oContacto = new Contacto();
+                    oContacto.nombre = txtNombre.Text;
+                    oContacto.apellido = txtApellido.Text;
+                    if (oContacto.Guardar())
+                    {
+                        MessageBox.Show("El Contacto se agrego con exito!!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se ha podido Guardar el contacto");
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    MessageBox.Show("No se ha podido Guardar el contacto");
+
+                    MessageBox.Show(ex.Message);
                 }
             }
-            catch (Exception ex)
+            else
             {
-
-                MessageBox.Show(ex.Message);
+                MessageBox.Show("Complete los campos, por favor!");
             }
            
         }
@@ -48,19 +54,23 @@ namespace NetAgenda
             Contacto oContacto = new Contacto();
             dgvGrillaDeContactos.DataSource = oContacto.ListarTabla();
 
-            cmbListar.DataSource = oContacto.ListarTabla();
+            cmbListar.DataSource = oContacto.Listar();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            cmbListar.DisplayMember = "apellido"; 
-            cmbListar.ValueMember = "id";
+            //cmbListar.DisplayMember = "apellido"; 
+            //cmbListar.ValueMember = "codigo"; comento sino no funciona obviamente el override al tostring()
         }
 
         private void btnSeleccion_Click(object sender, EventArgs e)
         {
-            int idSeleccionado =  (int)cmbListar.SelectedValue;
-            MessageBox.Show("el valor seleccionado es: " + idSeleccionado);
+            //int idSeleccionado =  (int)cmbListar.SelectedValue;
+            //MessageBox.Show("el valor seleccionado es: " + idSeleccionado);
+            //como mencione tengo mas plasticidad con objeto q con datatable
+            Contacto oContacto = (Contacto)cmbListar.SelectedItem;
+            MessageBox.Show("El codigo es: " + oContacto.codigo + " cuyo nombre y apellido es: " + oContacto.nombre + " " + oContacto.apellido);
+        
         }
     }
 }
